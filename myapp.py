6bd5564f2da7fe379a661f9298b685f32478c563
Ambush3 <aaronbush3@gmail.com>
2021-11-15 18:50:29 -0500

    Cleaning up csv file

--- myapp.py
+++ myapp.py
@@ -1,4 +1,5 @@
 import dropbox
+
 access_token = '=sl.A8OmJb6Y_9idMBTuLkNfqMTtKCUn90TAoA-p9veqb9h5wA6UcdZC7zrO_xf44wXxGxinsHpa5EnAp_AbpE--RZMSAqmhoCcYWdsFHz3D9Jc7K1qGcd9jKnGVfkLYhjV83BDqK-w'
 file_from = 'F:\PyCharm\WebScraping\Library\KDL.ipynb'
 file_to = 'https://www.dropbox.com/home/KDLLibraryScrape'
